2ed3e637565f0bad93c635058818d8af0a6f39f3
dlxx <dlxxdlcc@gmail.com>
2023-08-08 03:32:20 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <InputMessage
+    v-for="post in posts"
+    :key="post.id"
+    :address="post.address"
+  ></InputMessage>
+</template>
+
+<script>
+
+import InputMessage from "./InputMessage.vue";
+export default {
+  components: {
+    InputMessage,
+  },
+  data() {
+    return {
+      posts: [
+        { id: 1, address: "http://www.SBliuyangqing.com" },          // <---地址写在这里
+        { id: 2, address: "xxxx" },
+        { id: 3, address: "xxxx" },
+        { id: 4, address: "xxxx" },
+        { id: 5, address: "xxxx" },
+        { id: 6, address: "xxxx" },
+        { id: 7, address: "xxxx" },
+        { id: 8, address: "xxxx" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/InputMessage.vue
@@ -0,0 +1,42 @@
+<template>
+  <form onsubmit="return false">
+    <p>地址:</p>
+    <input placeholder="edit me" type="text" v-model="message"/>
+    <button @click="clickButton">click</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  props: ["address"],
+  data() {
+    return {
+      message: "",
+    };
+  },
+  methods: {
+    clickButton() {
+      console.log(this.message);
+      axios({
+        method: "post",
+        url: this.address,
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        data: this.message,
+      })
+        .then((res) => {
+          console.log(res);
+
+        })
+        .catch((err) => {
+          console.log(err.response);
+        });
+        
+    },
+  },
+};
+</script>
+
+<style></style>
